ad645fa8a977b57c97a049268fd3c25627885e5a
duanzhifen <duanzhifeng@qingfengweb.com>
2018-08-24 21:48:06 +0800

    Signed-off-by: duanzhifen <duanzhifeng@qingfengweb.com>

--- src/main/java/com/example/adminletdemo/IndexController.java
+++ src/main/java/com/example/adminletdemo/IndexController.java
@@ -25,59 +25,95 @@ import com.example.adminletdemo.page.PageInfo;
 
 @RestController
 @RequestMapping("/index/")
-//@Slf4j
+// @Slf4j
 public class IndexController {
 	private Logger log = LoggerFactory.getLogger(getClass());
-	
-    @RequestMapping("find")
-    public PageInfo<DemoEntity> findData(@RequestBody JSONObject data){
-        log.info("find data's "+data);
-        List<DemoEntity> list = new ArrayList<>(10);
-        for (int i=0;i<5;i++){
-            DemoEntity demo = new DemoEntity();
-            demo.setId((long)i);
-            demo.setLoginId("099"+i);
-            demo.setUserName("张"+(i+1));
-            demo.setCompanyName("上海润迅");
-            demo.setDeptName("研发部");
-            demo.setSex("女");
-            demo.setIsAdmin("是");
-            demo.setIsStart("是");
-            demo.setUpdateTime(new Date());
-            demo.setPhoneNumber("17718181919");
-            list.add(demo);
-        }
-        return new PageInfo<>(list);
-    }
 
-    @PostMapping
-    public Map save(DemoEntity data){
-        log.info("save data's "+data);
-        Map m = new HashMap();
-        m.put("success",true);
-        return m;
-    }
-    @GetMapping(path = "/{id:.+}")
-    public DemoEntity queryOne(String id){
-        log.info("query id's "+id);
-        DemoEntity demo = new DemoEntity();
-        demo.setId(33l);
-        demo.setLoginId("091");
-        demo.setUserName("张三");
-        demo.setCompanyName("上海润迅");
-        demo.setDeptName("研发部");
-        demo.setSex("女");
-        demo.setIsAdmin("否");
-        demo.setIsStart("否");
-        demo.setPhoneNumber("17718181919");
-        demo.setUpdateTime(new Date());
-        return demo;
-    }
-    @DeleteMapping(path = "/{id:.+}")
-    public Map delete(@PathVariable Long id){
-        log.info("del id's "+id);
-        Map m = new HashMap();
-        m.put("success",true);
-        return m;
-    }
+	@RequestMapping("find")
+	public PageInfo<DemoEntity> findData(@RequestBody JSONObject data) {
+		log.info("find data's " + data);
+		List<DemoEntity> list = new ArrayList<>(10);
+		for (int i = 0; i < 5; i++) {
+			DemoEntity demo = new DemoEntity();
+			demo.setId((long) i);
+			demo.setLoginId("099" + i);
+			demo.setUserName("张" + (i + 1));
+			demo.setCompanyName("上海润迅");
+			demo.setDeptName("研发部");
+			demo.setSex("女");
+			demo.setIsAdmin("是");
+			demo.setIsStart("是");
+			demo.setUpdateTime(new Date());
+			demo.setPhoneNumber("17718181919");
+			list.add(demo);
+		}
+		return new PageInfo<>(list);
+	}
+
+	@RequestMapping("findLog")
+	public PageInfo<LogEntity> findLogData(@RequestBody JSONObject data) {
+		log.info("find data's " + data);
+		List<LogEntity> list = new ArrayList<>(10);
+		for (int i = 0; i < 5; i++) {
+			LogEntity demo = new LogEntity();
+			demo.setLogid(i);
+			demo.setLoginId("099" + i);
+			demo.setUserName("张" + (i + 1));
+			demo.setDeptName("研发部");
+			demo.setLoginIp("192.168.0.1");
+			demo.setUrl("http://localhost:8080/log.html");
+			demo.setMenuName("大屏显示");
+			demo.setUpdateTime(new Date());
+			list.add(demo);
+		}
+		return new PageInfo<>(list);
+	}
+	@RequestMapping("findRole")
+	public PageInfo<RoleEntity> findRoleData(@RequestBody JSONObject data) {
+		log.info("find data's " + data);
+		List<RoleEntity> list = new ArrayList<>(10);
+		for (int i = 0; i < 5; i++) {
+			RoleEntity demo = new RoleEntity();
+			demo.setRoleId(i);
+			demo.setRoleName("张" + (i + 1));
+			demo.setRemark("研发部人的角色信息");
+			demo.setUpdateName("张三");
+			demo.setUpdateTime(new Date());
+			list.add(demo);
+		}
+		return new PageInfo<>(list);
+	}
+
+	@PostMapping
+	public Map save(DemoEntity data) {
+		log.info("save data's " + data);
+		Map m = new HashMap();
+		m.put("success", true);
+		return m;
+	}
+
+	@GetMapping(path = "/{id:.+}")
+	public DemoEntity queryOne(String id) {
+		log.info("query id's " + id);
+		DemoEntity demo = new DemoEntity();
+		demo.setId(33l);
+		demo.setLoginId("091");
+		demo.setUserName("张三");
+		demo.setCompanyName("上海润迅");
+		demo.setDeptName("研发部");
+		demo.setSex("女");
+		demo.setIsAdmin("否");
+		demo.setIsStart("否");
+		demo.setPhoneNumber("17718181919");
+		demo.setUpdateTime(new Date());
+		return demo;
+	}
+
+	@DeleteMapping(path = "/{id:.+}")
+	public Map delete(@PathVariable Long id) {
+		log.info("del id's " + id);
+		Map m = new HashMap();
+		m.put("success", true);
+		return m;
+	}
 }
---
+++ src/main/java/com/example/adminletdemo/RoleEntity.java
@@ -0,0 +1,42 @@
+package com.example.adminletdemo;
+
+import java.util.Date;
+
+public class RoleEntity {
+	private int roleId;
+	private String roleName;
+	private String remark;
+	private Date updateTime;
+	private String updateName;
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+	public String getUpdateName() {
+		return updateName;
+	}
+	public void setUpdateName(String updateName) {
+		this.updateName = updateName;
+	}
+	
+}
